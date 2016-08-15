bf6a76fc388fbb87bbeef9e68417e3b0bc391c6c
Laser Bandera <sfera.laser@gmail.com>
2016-08-15 16:36:17 +0300

    authorisation + userlogin + register design fix

--- blog/forms.py
+++ blog/forms.py
@@ -5,7 +5,7 @@ from registration.forms import User
 class SimpleForm(forms.Form):
     username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'size': 40, 'class': 'form-control'}))
     sender = forms.EmailField(widget=forms.TextInput(attrs={'size': 40, 'class': 'form-control'}))
-    message = forms.CharField(widget=forms.Textarea(attrs={'class': 'form- control'}))
+    message = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}))
     copy = forms.BooleanField(required=False)
 
 class UserForm(forms.ModelForm):
