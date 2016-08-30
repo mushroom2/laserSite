0056a51036a2203a248fc73e4dc300f39d39e1f5
Laser Bandera <sfera.laser@gmail.com>
2016-08-30 16:00:58 +0300

    little language fix

--- cart1/forms.py
+++ cart1/forms.py
@@ -5,7 +5,8 @@ PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
 
 class CartAddProductsForm(forms.Form):
     quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES,
-                                      coerce=int)
+                                      coerce=int,
+                                      label='Кількість ')
     update = forms.BooleanField(required=False,
                                 initial=False,
                                 widget=forms.HiddenInput)
